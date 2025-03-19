62afb4cef1bfaddb33cc9c89d76681f5250a910a
ale499 <gimenezalejandro083@gmail.com>
2025-03-19 19:10:19 -0300

    primer commit

---
+++ src/main/java/com/example/BackBuenSabor/BackBuenSaborApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackBuenSabor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBuenSaborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackBuenSaborApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BackBuenSabor/BackBuenSaborApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BackBuenSabor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBuenSaborApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
